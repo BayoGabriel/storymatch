476883677a978481a0c20eadd1e97c511eca937f
Bayode Gabriel <bayogabriel24@gmail.com>
2024-04-26 11:08:32 +0000

    Apr 26, 2024, 12:08 PM

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Yayy = () => {
   return (
     <>
-        <div className="w-full h-screen items-center justify-center">
+        <div className="w-screen h-screen items-center justify-center">
             <h5 className="font-bold text-center">Wooooh Wooooh Woooh</h5>
             <p className="mt-8">I am yet to write this page man <br /> Slow down</p>
         </div>
