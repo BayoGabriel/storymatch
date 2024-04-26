1d5c1e63cacbd7f708a64a32e438f897904f1c3f
Bayode Gabriel <bayogabriel24@gmail.com>
2024-04-26 10:10:24 +0000

    Apr 26, 2024, 11:10 AM

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 const withMT = require("@material-tailwind/react/utils/withMT");
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      colors: {
-        'ddarkest': '#1D1F1F',
-        'ddarker': '#5D5F5F',
-        'dgrey': '#B6B8B8',
-        'dlighter': '#E3E3E3',
-        'dlightest': '#F4F4F5',
+// module.exports = {
+//   content: [
+//     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {
+//       backgroundImage: {
+//         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+//         "gradient-conic":
+//           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+//       },
+//       colors: {
+//         'ddarkest': '#1D1F1F',
+//         'ddarker': '#5D5F5F',
+//         'dgrey': '#B6B8B8',
+//         'dlighter': '#E3E3E3',
+//         'dlightest': '#F4F4F5',
 
-        'darkest': '#DE5212',
-        'darker': '#E9672B',
-        'normal': '#EF8C5F',
-        'lighter': '#E8CFB5',
-        'lightest': '#FBF8F5',
+//         'darkest': '#DE5212',
+//         'darker': '#E9672B',
+//         'normal': '#EF8C5F',
+//         'lighter': '#E8CFB5',
+//         'lightest': '#FBF8F5',
 
-        'ared': '#DA3E31',
-        'ayellow': '#CDD454',
-        'agreen': '#263437'
-      }
-    },
-  },
-  plugins: [],
-};
+//         'ared': '#DA3E31',
+//         'ayellow': '#CDD454',
+//         'agreen': '#263437'
+//       }
+//     },
+//   },
+//   plugins: [],
+// };
