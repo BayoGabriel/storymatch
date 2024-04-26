6c4d405d619f654b0405942eabfbe1c2473b6978
Bayode Gabriel <bayogabriel24@gmail.com>
2024-04-26 07:45:35 +0000

    Apr 26, 2024, 8:45 AM

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -5,7 +5,7 @@ const Yayy = () => {
     <>
         <div className="w-full h-screen items-center justify-center">
             <h5 className="font-bold">Wooooh Wooooh Woooh</h5>
-            <p className="mt-8">I am yet to write this page man <br /> Slow </p>
+            <p className="mt-8">I am yet to write this page man <br /> Slow down</p>
         </div>
     </>
   )
