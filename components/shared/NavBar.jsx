0d2e712e685b298e7de5a1f3e2668d60ade5513e
Bayode Gabriel <bayogabriel24@gmail.com>
2024-04-26 09:56:51 +0000

    Apr 26, 2024, 10:56 AM

--- components/shared/NavBar.jsx
+++ components/shared/NavBar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
       <Link href="/" className="text-darkest">
         LOGO
       </Link>
-      <Link>Explore</Link>
+      <Link>J</Link>
       </div>
         <div className="hidden lg:flex relative items-center">
           <input
