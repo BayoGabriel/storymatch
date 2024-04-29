c238617c25026329432f065258267663a5c4e885
Bayode Gabriel <bayogabriel24@gmail.com>
2024-04-29 12:32:17 +0000

    auto-commit

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
                         </h1>
                     </div>
                     <div className="max-lg:hidden flex gap-4">
-                        <Image
+                        <Image/>
                     </div>
                 </div>
             </Carousel>
