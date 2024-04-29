2dc295df70bb28ec1d6ac7e4fcef16e50ecd5933
Bayode Gabriel <bayogabriel24@gmail.com>
2024-04-29 13:06:04 +0000

    auto-commit

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <>
         <div className="w-full h-[89vh]">
-            <Carousel leftControl=" " rightControl=" " indicators={false} pauseOnHover className='h-full'>
+            <Carousel leftControl=" " rightControl=" " indicators={false} pauseOnHover className='h-full rounded-none'>
                 <div className="w-full h-full idan rounded-none bg-red-200 text-black">
                     <div className="flex w-full h-full items-center justify-center bg-yellow-300 flex-col gap-8">
                         <p>From Anecdotes to Anomalies, Find Your Tribe and Talk Freely.</p>
