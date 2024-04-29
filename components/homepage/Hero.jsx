470f02857f45a9a9cce2a4b439bfedaebec9484b
Bayode Gabriel <bayogabriel24@gmail.com>
2024-04-29 10:58:53 +0100

    auto-commit

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -1,28 +1,49 @@
-import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import './style.css';
 
-const Hero = () => {
+const TextAnimation = () => {
+  const [index, setIndex] = useState(0);
+  const texts = [
+    'Wear Face Mask',
+    'Cover Face When Coughing',
+    'Wash Your Hand Frequently',
+    'Stay At Home',
+    'Avoid Close Contact'
+  ];
+
+  const textInTimer = 3000;
+  const textOutTimer = 2800;
+
+  useEffect(() => {
+    const animateText = () => {
+      const animateInterval = setInterval(() => {
+        setIndex(prevIndex => (prevIndex === texts.length - 1 ? 0 : prevIndex + 1));
+      }, textInTimer + textOutTimer);
+
+      return () => clearInterval(animateInterval);
+    };
+
+    animateText();
+  }, []);
+
   return (
-    <>
-        <div className="h-full w-full carousel carousel-vertical carousel-start rounded-box">
-            <div className="carousel-item h-full w-full bg-red-400">
-                
-            </div> 
-            <div className="carousel-item h-full w-full bg-green-400 ">
-            </div> 
-            <div className="carousel-item h-full w-full bg-blue-400 ">
-            </div> 
-            <div className="carousel-item h-full w-full bg-orange-400 ">
-            </div> 
-            <div className="carousel-item h-full w-full bg-red-900">
-             </div> 
-            <div className="carousel-item h-full w-full bg-yellow-400">
-             </div> 
-            <div className="carousel-item h-full w-full bg-blue-900 ">
-            </div>
+    <div className="home">
+      <div className="container">
+        <div className="row">
+          <div className="home-text">
+            <h1>How can I protect myself from Covid-19?</h1>
+            <p className="animate-text">
+              {texts.map((text, idx) => (
+                <span key={idx} className={idx === index ? 'text-in' : 'text-out'}>
+                  {text}
+                </span>
+              ))}
+            </p>
+          </div>
         </div>
-    </>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default TextAnimation;
