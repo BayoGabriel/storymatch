dad04525e0c80a43969bf5002db104e8179985f4
Bayode Gabriel <bayogabriel24@gmail.com>
2024-04-29 10:20:17 +0000

    auto-commit

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Hero = () => {
@@ -5,11 +6,11 @@ const Hero = () => {
     <>
         <div className="w-full h-[500px]">
         <Carousel pauseOnHover>
-        <Ima src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
-        <Ima src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
-        <Ima src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
-        <Ima src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
-        <Ima src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+        <Image src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
+        <Image src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
+        <Image src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
+        <Image src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
+        <Image src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
       </Carousel>
         </div>
     </>
