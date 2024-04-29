9c699e1046f186a63feffd3d1d9e7e62a6803e4b
Bayode Gabriel <bayogabriel24@gmail.com>
2024-04-29 13:06:00 +0000

    Apr 29, 2024, 2:06 PM

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
                         Instant Connection, Endless Conversations
                         </h1>
                     </div>
-                    <div className="w-full"></div>
+                    <div className="w-full hfull"></div>
                 </div>
             </Carousel>
         </div>
