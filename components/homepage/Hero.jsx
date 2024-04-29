36f26b5ce71b09688acc545aaf28efc071614850
Bayode Gabriel <bayogabriel24@gmail.com>
2024-04-29 10:15:37 +0000

    Apr 29, 2024, 11:15 AM

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useEffect, useState } from 'react';
-import '@/public/styles';
+import '@/public/styles/';
 
 const TextAnimation = () => {
   const [index, setIndex] = useState(0);
