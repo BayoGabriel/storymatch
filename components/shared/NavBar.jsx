cbc6984d1d19d539bcc899ce323e20098d5f3c59
Bayode Gabriel <bayogabriel24@gmail.com>
2024-04-29 10:03:47 +0000

    auto-commit

--- components/shared/NavBar.jsx
+++ components/shared/NavBar.jsx
@@ -4,7 +4,7 @@ import { CiSearch, CiHeart, CiMenuFries, CiShoppingCart, CiUser } from "react-ic
 import { useState } from 'react';
 import Link from "next/link";
 import Image from "next/image";
-import logo from '@/public/imageslogo.png'
+import logo from '@/public/images/logo.png'
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
