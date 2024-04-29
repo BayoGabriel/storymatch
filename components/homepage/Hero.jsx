95fda28574cc071b854d3efb9b5b8d660ae67810
Bayode Gabriel <bayogabriel24@gmail.com>
2024-04-29 13:06:04 +0000

    auto-commit

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
                         Instant Connection, Endless Conversations
                         </h1>
                     </div>
-                    <div className="w-full h-full"></div>
+                    <div className="w-full h-full grid grid-cols-4 grid-rows"></div>
                 </div>
             </Carousel>
         </div>
