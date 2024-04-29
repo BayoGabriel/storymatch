01940a5ed1114ae73a3610d0e91543ad66ad8a50
Bayode Gabriel <bayogabriel24@gmail.com>
2024-04-29 10:47:20 +0000

    auto-commit

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <div className="w-full h-[500px]">
             <Carousel leftControl=" " rightControl=" " indicators={false} pauseOnHover>
                 <div className="w-full h-full bg-green-700 text-white">
-                    .h5
+                    h1.
                 </div>
                 <div className="w-full h-full bg-green-800"></div>
                 <div className="w-full h-full bg-green-900 poppins-extrabold text-white">
