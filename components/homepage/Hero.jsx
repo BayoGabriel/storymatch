63d8b6574b2a5eee8a65b5351926aa52914ffac8
Bayode Gabriel <bayogabriel24@gmail.com>
2024-04-29 10:51:24 +0000

    auto-commit

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -10,7 +10,6 @@ const Hero = () => {
                     <h1 className="text-[50px]">
                     Instant Connection, Endless Conversations
                     </h1>
-                    <p>From Anecdotes to Anomalies, Find Your Tribe and Talk Freely.</p>
                 </div>
                 <div className="w-full flex items-center justify-center flex-col gap-8 h-full bg-green-800 text-white">
                     <p>Dive Straight into the Deep End of Chats That Speak to You.</p>
