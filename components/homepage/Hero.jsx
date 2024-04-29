7e83278ee0fad70ef0cb19506e90d8032a2ad5e6
Bayode Gabriel <bayogabriel24@gmail.com>
2024-04-29 06:51:23 +0000

    auto-commit

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <>
         <div className="h-full carousel carousel-vertical rounded-box">
-            <div className="carousel-item h-full bg-red">
+            <div className="carousel-item h-full bg-red-400">
                 
             </div> 
             <div className="carousel-item h-full">
