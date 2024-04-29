b1c9d73fb7d62b4481ed05001adc18c3bcf934a6
Bayode Gabriel <bayogabriel24@gmail.com>
2024-04-29 11:43:34 +0000

    Apr 29, 2024, 12:43 PM

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
                     Instant Connection, Endless Conversations
                     </h1>
                 </div>
-                <div className="w-full flex items-center justify-center flex-col gap-8 h-full bg-green-800 text-white">
+                <div className="w-full flex items-center justify-center flex-col gap-8 h-full bg-green-800 rounded-none text-white">
                     <p>Dive Straight into the Deep End of Chats That Speak to You.</p>
                     <h1 className='text-[50px]'>No Icebreakers Needed</h1>
                     
