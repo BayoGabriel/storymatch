00a5643487508e70cad14621a27cef3820c189ba
Bayode Gabriel <bayogabriel24@gmail.com>
2024-04-29 12:46:40 +0000

    auto-commit

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
                         </h1>
                     </div>
                     <div className="w-full flex items-center gap-2">
-                        .
+                        <div className=""></div>
                     </div>
                 </div>
             </Carousel>
