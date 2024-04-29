e57640e9b4f3658f099c1d39cfdc20ed1321e124
Bayode Gabriel <bayogabriel24@gmail.com>
2024-04-29 10:15:35 +0000

    auto-commit

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useEffect, useState } from 'react';
-import '@/public//hero.css';
+import '@/public/styles';
 
 const TextAnimation = () => {
   const [index, setIndex] = useState(0);
