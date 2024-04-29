9f928bf3f41ea100522a6d5e49be756e68af1025
Bayode Gabriel <bayogabriel24@gmail.com>
2024-04-29 10:20:17 +0000

    auto-commit

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -6,11 +6,7 @@ const Hero = () => {
     <>
         <div className="w-full h-[500px]">
         <Carousel pauseOnHover>
-        <Image src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
-        <Image src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
-        <Image src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
-        <Image src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
-        <Image src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+        
       </Carousel>
         </div>
     </>
