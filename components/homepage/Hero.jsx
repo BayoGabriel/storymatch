6a2a589aeba9e93ce7c4e3692c298e4c9b87ed5a
Bayode Gabriel <bayogabriel24@gmail.com>
2024-04-30 12:57:05 +0000

    Apr 30, 2024, 1:57 PM

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
                         </h1>
                     </div>
                     <div className="w-full grid grid-cols-3 grid-rows-3">
-                        <div className="idan h"></div>
+                        <div className="idan h-["></div>
                     </div>
                 </div>
             </Carousel>
