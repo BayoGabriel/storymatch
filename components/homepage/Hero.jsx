6b0d2fb8b91268564d364d182c8a9b3b46e41889
Bayode Gabriel <bayogabriel24@gmail.com>
2024-04-30 12:57:11 +0000

    auto-commit

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
                         </h1>
                     </div>
                     <div className="w-full grid grid-cols-3 grid-rows-3">
-                        <div className="idan h-[60px]"></div>
+                        <div className="idan"></div>
                     </div>
                 </div>
             </Carousel>
