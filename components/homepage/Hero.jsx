85301f6a4256ffe36840e0962bc1c18702e5d7e5
Bayode Gabriel <bayogabriel24@gmail.com>
2024-04-30 14:02:13 +0100

    auto-commit

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -1,48 +1,20 @@
 import Image from 'next/image'
 import { Carousel } from 'flowbite-react'
 import '@/public/styles/hero.css'
-import one from '@/public/images/diego-ph-fIq0tET6llw-unsplash.jpg'
+
 const Hero = () => {
   return (
     <>
-        <div className="w-full h-[89vh] relative">
+        <div className="w-full h-[89vh]">
             <Carousel leftControl=" " rightControl=" " indicators={false} pauseOnHover className='h-full'>
-                <div className="w-full h-full  rounded-none py-6 bg-red-200 text-black">
-                    <div className="flex w-full h-full items-center justify-center z-40 flex-col gap-8 absolute top-10 ">
+                <div className="w-full h-full idan rounded-none py-6 bg-red-200 text-black">
+                    <div className="flex w-full h-full items-center justify-center bg-yellow-300 flex-col gap-8 ">
                         <p>From Anecdotes to Anomalies, Find Your Tribe and Talk Freely.</p>
                         <h1 className="text-[50px]">
                         Instant Connection, Endless Conversations
                         </h1>
                     </div>
-                    <div className="w-full grid grid-cols-3 grid-rows-3">
-                        <div className="">
-                            <Image src={one} alt='on' className='w-full h-full'/>
-                        </div>
-                        <div className="">
-                            <Image src={one} alt='on' className='w-full h-full'/>
-                        </div>
-                        <div className="">
-                            <Image src={one} alt='on' className='w-full h-full'/>
-                        </div>
-                        <div className="">
-                            <Image src={one} alt='on' className='w-full h-full'/>
-                        </div>
-                        <div className="">
-                            <Image src={one} alt='on' className='w-full h-full'/>
-                        </div>
-                        <div className="">
-                            <Image src={one} alt='on' className='w-full h-full'/>
-                        </div>
-                        <div className="">
-                            <Image src={one} alt='on' className='w-full h-full'/>
-                        </div>
-                        <div className="">
-                            <Image src={one} alt='on' className='w-full h-full'/>
-                        </div>
-                        <div className="">
-                            <Image src={one} alt='on' className='w-full h-full'/>
-                        </div>
-                    </div>
+                  
                 </div>
             </Carousel>
         </div>
