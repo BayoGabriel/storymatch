85b5daca8b12cc76ce3ed3bcdb937a1d938f5b38
Bayode Gabriel <bayogabriel24@gmail.com>
2024-04-30 12:57:11 +0000

    auto-commit

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -16,6 +16,14 @@ const Hero = () => {
                     </div>
                     <div className="w-full grid grid-cols-3 grid-rows-3">
                         <div className="idan h-[60px]"></div>
+                        <div className="idan h-[60px]"></div>
+                        <div className="idan h-[60px]"></div>
+                        <div className="idan h-[60px]"></div>
+                        <div className="idan h-[60px]"></div>
+                        <div className="idan h-[60px]"></div>
+                        <div className="idan h-[60px]"></div>
+                        <div className="idan h-[60px]"></div>
+                        <div className="idan h-[60px]"></div>
                     </div>
                 </div>
             </Carousel>
