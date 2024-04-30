030da1445bb1b90e69d2dd90c49823c204e6d672
Bayode Gabriel <bayogabriel24@gmail.com>
2024-04-30 13:59:13 +0100

    auto-commit

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
                     </div>
                     <div className="w-full grid grid-cols-3 grid-rows-3">
                         <div className="idan">
-                            <Image src={one} alt='on' className=''/>
+                            <Image src={one} alt='on' className='w-full h-full'/>
                         </div>
                     </div>
                 </div>
