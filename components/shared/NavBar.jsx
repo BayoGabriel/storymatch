11c452bd321701ed57e73dfc49d3ea4e33671673
Bayode Gabriel <bayogabriel24@gmail.com>
2024-05-01 07:11:15 +0000

    auto-commit

--- components/shared/NavBar.jsx
+++ components/shared/NavBar.jsx
@@ -39,7 +39,6 @@ const Navbar = () => {
       <Link href="/" className="text-agreen h-[50px] ">
         Logo
       </Link>
-      <Link href="/">Explore</Link>
       </div>
         <div className="hidden lg:flex relative items-center">
           <input
