9a1da68665caac8fe763f95daf756074f0f813f1
Bayode Gabriel <bayogabriel24@gmail.com>
2024-05-01 08:08:15 +0100

    auto-commit

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -7,12 +7,12 @@ const Hero = () => {
     <>
         <div className="w-full h-[89vh]">
             <Carousel leftControl=" " rightControl=" " indicators={false} pauseOnHover className='w-full h-full idan rounded-none py-6 bg-red-200 text-black'>
-                    <div className="flex w-full h-full items-center justify-center bg-yellow-300 flex-col gap-8 ">
-                        <p>From Anecdotes to Anomalies, Find Your Tribe and Talk Freely.</p>
-                        <h1 className="text-[50px]">
-                        Instant Connection, Endless Conversations
-                        </h1>
-                    </div>
+                <div className="flex w-full h-full items-center justify-center bg-yellow-300 flex-col gap-8 ">
+                    <p>From Anecdotes to Anomalies, Find Your Tribe and Talk Freely.</p>
+                    <h1 className="text-[50px]">
+                    Instant Connection, Endless Conversations
+                    </h1>
+                </div>
             </Carousel>
         </div>
     </>
