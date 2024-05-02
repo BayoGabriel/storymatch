1f72eb65c0204ef744740ccfcd1366a6acbceed5
Bayode Gabriel <bayogabriel24@gmail.com>
2024-05-02 11:33:47 +0000

    auto-commit

--- components/homepage/About.jsx
+++ components/homepage/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <>
         <div className="p-4 w-full">
-            <h1 className="text-black">HOW IT WORKS</h1>
+            <h1 className="text-black">HOW IT WORKS </h1>
         </div>
     </>
   )
