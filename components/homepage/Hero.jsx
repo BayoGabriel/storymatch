c074dc80c897ad946276e074db6fd40d4688865c
Bayode Gabriel <bayogabriel24@gmail.com>
2024-05-02 11:10:36 +0000

    auto-commit

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
                     </h1>
                 </div>
                 <div className="grid">
-                    <Image src={} alt=''
+                    <Image src={} alt='alt'
                 </div>
                 </div>
                 <div className="flex idan w-full h-full items-center justify-center bg-red-300 flex-col gap-8 ">
