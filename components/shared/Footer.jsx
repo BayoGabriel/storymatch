93c376c257df4009a0da437f00b03a64eccd31f0
Bayode Gabriel <bayogabriel24@gmail.com>
2024-05-02 15:45:41 +0000

    auto-commit

--- components/shared/Footer.jsx
+++ components/shared/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='flex items-center justify-center p-4 mb-'>This will be a footer just to point to links, like that of the menu</div>
+    <div className='flex items-center justify-center p-4 mb-8'>This will be a footer just to point to links, like that of the menu</div>
   )
 }
 
