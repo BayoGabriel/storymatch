b646ebf26142bba8ebc005a646574acac6e9c419
Bayode Gabriel <bayogabriel24@gmail.com>
2024-05-02 12:14:25 +0000

    auto-commit

--- components/homepage/About.jsx
+++ components/homepage/About.jsx
@@ -5,11 +5,11 @@ const About = () => {
     <>
         <div className="p-4 w-full flex flex-col items-center justify-center">
             <h1 className="text-black mb-10">HOW IT WORKS </h1>
-            <div className="w-full grid grid-cols-3 max-lg:grid-cols-1">
+            {/* <div className="w-full grid grid-cols-3 max-lg:grid-cols-1">
                 <div className="p-[30px] bg-slate-300 text-black flex items-center justify-center flex-col">
                     <div className="p-3 rounded-full flex items-center justify-center text-darkest bg-white"><CgProfile/></div>
                 </div>
-            </div>
+            </div> */}
         </div>
     </>
   )
