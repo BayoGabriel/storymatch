536746384dc1b807e4c59bd5cf4927144a51f3ad
Bayode Gabriel <bayogabriel24@gmail.com>
2024-05-02 12:01:06 +0100

    auto-commit

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -13,13 +13,13 @@ const Hero = () => {
                     Instant Connection, Endless Conversations
                     </h1>
                 </div>
-                <div className="flex w-full h-full items-center justify-center bg-red-300 flex-col gap-8 ">
+                <div className="flex idan w-full h-full items-center justify-center bg-red-300 flex-col gap-8 ">
                     <p>From Anecdotes to Anomalies, Find Your Tribe and Talk Freely.</p>
                     <h1 className="text-[50px]">
                     Carousel 2
                     </h1>
                 </div>
-                <div className="flex w-full h-full items-center justify-center bg-blue-300 flex-col gap-8 ">
+                <div className="flex idan w-full h-full items-center justify-center bg-blue-300 flex-col gap-8 ">
                     <p>From Anecdotes to Anomalies, Find Your Tribe and Talk Freely.</p>
                     <h1 className="text-[50px]">
                     Carousel 3
