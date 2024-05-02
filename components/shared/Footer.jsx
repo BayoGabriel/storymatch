5a996713e537d82db70566cf03212ec52b629ca5
Bayode Gabriel <bayogabriel24@gmail.com>
2024-05-02 16:40:48 +0100

    auto-commit

--- components/shared/Footer.jsx
+++ components/shared/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>This will be a footer just to point to </div>
+    <div>This will be a footer just to point to links, lik</div>
   )
 }
 
