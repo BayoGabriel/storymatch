e38de0ca4d8d2d3e464e809f12b4f0b1b31ca4ae
Bayode Gabriel <bayogabriel24@gmail.com>
2024-05-02 12:27:08 +0100

    auto-commit

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -8,19 +8,19 @@ const Hero = () => {
     <>
         <div className="w-full h-[89vh]">
             <Carousel leftControl=" " rightControl=" " indicators={false} pauseOnHover className='w-full h-full idan rounded-none  text-white'>
-                <div className="flex idan w-full h-full items-center justify-center bg-red-300 flex-col gap-8 ">
+                <div className="flex w-full h-full items-center justify-center bg-red-300 flex-col gap-8 ">
                     <p>From Anecdotes to Anomalies, Find Your Tribe and Talk Freely.</p>
                     <h1 className="text-[50px]">
                     Instant Connection, Endless Conversations
                     </h1>
                 </div>
-                <div className="flex idan w-full h-full items-center justify-center bg-red-300 flex-col gap-8 ">
+                <div className="flex w-full h-full items-center justify-center bg-red-300 flex-col gap-8 ">
                     <p>From Anecdotes to Anomalies, Find Your Tribe and Talk Freely.</p>
                     <h1 className="text-[50px]">
                     Carousel 2
                     </h1>
                 </div>
-                <div className="flex idan w-full h-full items-center justify-center bg-blue-300 flex-col gap-8 ">
+                <div className="flex w-full h-full items-center justify-center bg-blue-300 flex-col gap-8 ">
                     <p>From Anecdotes to Anomalies, Find Your Tribe and Talk Freely.</p>
                     <h1 className="text-[50px]">
                     Carousel 3
