f88e2f2dbbd0cce8634ed13f35ffc809264bf493
Bayode Gabriel <bayogabriel24@gmail.com>
2024-05-02 12:49:00 +0000

    auto-commit

--- components/shared/NavBar.jsx
+++ components/shared/NavBar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
   ]
   
   return (
-    <nav className="flex justify-between items-center ">
+    <nav className="flex justify-between items-center bg-transparent w-full px-6 py-2 mb-4">
     <div className="flex items-center justify-center">
     <Link href="/" className="text-red h-[50px] ">
       Trybe
