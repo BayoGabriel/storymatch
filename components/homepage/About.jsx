87e7986313f0d382edc3901794b1a4fe405ef8ca
Bayode Gabriel <bayogabriel24@gmail.com>
2024-05-02 11:46:51 +0000

    auto-commit

--- components/homepage/About.jsx
+++ components/homepage/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <>
-        <div className="p-4 w-full flex items">
+        <div className="p-4 w-full flex items-center ">
             <h1 className="text-black">HOW IT WORKS </h1>
         </div>
     </>
