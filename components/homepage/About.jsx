20668ed37759c9705c0c3e6971cb1980fe83caa4
Bayode Gabriel <bayogabriel24@gmail.com>
2024-05-02 11:49:35 +0000

    May 2, 2024, 12:49 PM

--- components/homepage/About.jsx
+++ components/homepage/About.jsx
@@ -5,7 +5,9 @@ const About = () => {
     <>
         <div className="p-4 w-full flex items-center justify-center">
             <h1 className="text-black mb-10">HOW IT WORKS </h1>
-            <div className="w-full grid grid-cols-3 max-lg:grid-cols-1"></div>
+            <div className="w-full grid grid-cols-3 max-lg:grid-cols-1">
+                
+            </div>
         </div>
     </>
   )
