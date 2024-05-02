8cdd37d1f080971e607c36ca7a752cd7f7b71615
Bayode Gabriel <bayogabriel24@gmail.com>
2024-05-02 12:14:25 +0000

    auto-commit

--- components/homepage/About.jsx
+++ components/homepage/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
             <h1 className="text-black mb-10">HOW IT WORKS </h1>
             <div className="w-full grid grid-cols-3 max-lg:grid-cols-1">
                 <div className="p-[30px] bg-slate-300 text-black flex items-center justify-center flex-col">
-                    <div className="p-3 rounded-full flex items-center justify-center text-darkest bg-white"><Cg</div>
+                    <div className="p-3 rounded-full flex items-center justify-center text-darkest bg-white"><CgPr</div>
                 </div>
             </div>
         </div>
