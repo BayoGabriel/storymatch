a0d6a9babb8c22f708817de4fa40d73c546a44ec
Bayode Gabriel <bayogabriel24@gmail.com>
2024-05-02 13:12:49 +0100

    auto-commit

--- components/homepage/About.jsx
+++ components/homepage/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <>
-        <div className="p-4 w-full flex items-center justify-center">
+        <div className="p-4 w-full flex flex-col items-center justify-center">
             <h1 className="text-black mb-10">HOW IT WORKS </h1>
             <div className="w-full grid grid-cols-3 max-lg:grid-cols-1">
                 <div className="p-[30px] bg-slate-300 text-black flex items-center justify-center flex-col">
