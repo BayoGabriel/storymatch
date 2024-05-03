1c30b522cf57cf9f9751c404fb8bfea6f5642fe1
Bayode Gabriel <bayogabriel24@gmail.com>
2024-05-03 07:53:23 +0000

    auto-commit

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -15,7 +15,15 @@ const Hero = () => {
                     </h1>
                     <button className='text-white bg-darker py-2 px-6 rounded-md'>Join Now</button>
                 </div>
-                <div className="grid grid-cols-2 max-md:gri"></div>
+                <div className="grid grid-cols-2 max-md:grid-cols-1">
+                <div className="flex w-full h-full items-center justify-center bg-red-300 flex-col gap-8 ">
+                    <h1 className="text-[50px]">
+                    Instant Connection, Endless Conversations
+                    </h1>
+                    <p>From Anecdotes to Anomalies, Find Your Tribe and Talk Freely.</p>
+                    <button className='text-white bg-darker py-2 px-6 rounded-md'>Join Now</button>
+                </div>
+                </div>
                 
                 <div className="flex w-full h-full items-center justify-center bg-blue-300 flex-col gap-8 ">
                     <p>From Anecdotes to Anomalies, Find Your Tribe and Talk Freely.</p>
