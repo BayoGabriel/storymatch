fda6a0ed76b58b4ffd018d24a760c104cfdaefeb
Bayode Gabriel <bayogabriel24@gmail.com>
2024-05-03 11:37:05 +0100

    auto-commit

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
                         <button className=''><FcGoogle/> </button>
                         </div>
                         <div className='text-white bg-darker py-2 px-6 rounded-md'>
-                        <button className=''><IoMailOpenOutline/> <span></span></button>
+                        <button className=''><IoMailOpenOutline/> <span>Join with </span></button>
                         </div>
                     </div>
                 </div>
