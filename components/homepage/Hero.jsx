86143514ceeb0a6d714600f38d7bfa265f038a96
Bayode Gabriel <bayogabriel24@gmail.com>
2024-05-03 07:53:05 +0000

    May 3, 2024, 8:53 AM

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -15,13 +15,7 @@ const Hero = () => {
                     </h1>
                     <button className='text-white bg-darker py-2 px-6 rounded-md'>Join Now</button>
                 </div>
-                <div className="flex w-full h-full items-center justify-center bg-red-300 flex-col gap-8 ">
-                    <h1 className="text-[50px]">
-                    Instant Connection, Endless Conversations
-                    </h1>
-                    <p>From Anecdotes to Anomalies, Find Your Tribe and Talk Freely.</p>
-                    <button className='text-white bg-darker py-2 px-6 rounded-md'>Join Now</button>
-                </div>
+                .
                 
                 <div className="flex w-full h-full items-center justify-center bg-blue-300 flex-col gap-8 ">
                     <p>From Anecdotes to Anomalies, Find Your Tribe and Talk Freely.</p>
