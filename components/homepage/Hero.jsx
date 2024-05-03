99ce07d6ff0bc7658e7d23228adc91f92c4620c5
Bayode Gabriel <bayogabriel24@gmail.com>
2024-05-03 10:21:15 +0000

    auto-commit

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import { Carousel } from 'flowbite-react'
 import '@/public/styles/hero.css'
-import five from '@/public/images/kitchen.png'
+import five from '@/public/images/kitchen.jpg'
 
 const Hero = () => {
   return (
