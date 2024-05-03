6e22204d8a2b8c4a03dee043e4943f1a6c731b8a
Bayode Gabriel <bayogabriel24@gmail.com>
2024-05-03 10:11:32 +0000

    May 3, 2024, 11:11 AM

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
                     </h1>
                     <button className='text-white bg-darker py-2 px-6 rounded-md'>Join Now</button>
                 </div>
-                {/* <div className="grid grid-cols-2 max-md:grid-cols-1"> */}
+                <div className="grid grid-cols-2 max-md:grid-cols-1">
                 <div className="flex w-full h-full items-center justify-center bg-red-300 flex-col gap-8 ">
                     <h1 className="text-[50px]">
                     Instant Connection, Endless Conversations
