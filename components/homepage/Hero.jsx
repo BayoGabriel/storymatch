aa8ba8238749894bcc80f63ab319f5b39d858b65
Bayode Gabriel <bayogabriel24@gmail.com>
2024-05-03 10:19:59 +0000

    auto-commit

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
     <>
         <div className="w-full h-[89vh]">
             <Carousel leftControl=" " rightControl=" " indicators={false} pauseOnHover className='w-full h-full rounded-none  text-white'>
-                <div className="grid grid-cols-2 max-lg:grid"></div>
+                <div className="grid grid-cols-2 max-lg:grid-cols-1"></div>
                 <div className="grid grid-cols-2 max-md:grid-cols-1">
                 <div className="flex w-full h-full items-center justify-center bg-red-300 flex-col gap-8 ">
                     <h1 className="text-[50px]">
